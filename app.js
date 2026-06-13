4b5f60cbac6aefe0a3357a1e2859eff28a243af7
CB230494 <161616911+CB230494@users.noreply.github.com>
2026-06-13 12:19:53 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbz7NTgUWeC9-0FydoQzW1sCYJqhru4bOydL7itqGzEIAnd3RDSfF2y5ZTkLBQN45iCh/exec";
 
+let sesion = {
+  tipo: null,
+  persona: null
+};
+
+async function api(accion, datos = {}) {
+  const respuesta = await fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      accion,
+      ...datos
+    })
+  });
+
+  return await respuesta.json();
+}
+
+function guardarSesion(tipo, persona) {
+  sesion.tipo = tipo;
+  sesion.persona = persona;
+
+  localStorage.setItem("express_sesion", JSON.stringify(sesion));
+}
+
+function cargarSesion() {
+  const guardada = localStorage.getItem("express_sesion");
+
+  if (guardada) {
+    sesion = JSON.parse(guardada);
+  }
+}
+
+function cerrarSesion() {
+  localStorage.removeItem("express_sesion");
+  sesion = {
+    tipo: null,
+    persona: null
+  };
+
+  renderLogin();
+}
